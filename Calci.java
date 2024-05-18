6ab3e6466562894b371a1bd0f887f4f96d7f1a24
gopancoder <gopanav5@gmail.com>
2024-05-18 15:21:36 +0530

    Created main method and calling interface method

--- Calci.java
+++ Calci.java
@@ -6,4 +6,10 @@ class CalciImpl implements Calci {
 	public int add(int a, int b) {
 		return a + b;
 	}
+
+	public static void main(String[] args) {
+		CalciImpl c = new CalciImpl();
+		Sytem.out.println(c.add(5, 7));
+
+	}
 }
